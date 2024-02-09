9933e7fc5ca870dcb236268d85fc4d1eb6c373e1
Aditya Janager <58390543+Aditya-IIITD@users.noreply.github.com>
2024-02-09 15:11:04 +0530

    Update url.model.js

--- src/Model/url.model.js
+++ src/Model/url.model.js
@@ -3,7 +3,7 @@ import { ObjectId } from "mongodb";
 export default class UrlModel {
   constructor(url, key, userId) {
     this.originalUrl = url;
-    this.shortUrl = "https://url-shortner-2hea.onrender.com/" + key;
+    this.shortUrl = "https://url-shortner-2hea.onrender.com/bit/" + key;
     this.userId = new ObjectId(userId);
     this.visited = 0;
   }
